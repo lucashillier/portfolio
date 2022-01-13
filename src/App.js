45e5d6158be80be2ededfdb1bf6f19021704c0c4
Lucas Hillier <lucaskhillier@gmail.com>
2022-01-13 17:04:58 -0500

    Add bio

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import profilePic from './img/profile.jpg';
+import avatar from './img/profile.jpg';
 import './App.css';
 import { Layout, Sidebar, Content } from "./components/layout.js";
 import { Title, Subtitle, Body } from "./components/content.js";
 import { ButtonLink, Link } from "./components/link.js";
 import { Avatar } from "./components/image.js";
 
+const bio = require('./bio.json');
+
 function App() {
   return (
       <Layout>
         <Sidebar>
           <Title>Lucas Hillier</Title>
-          <Avatar src={profilePic} alt="Lucas Hillier"/>
-          <Body>A brief description</Body>
+          <Avatar src={avatar} alt="Lucas Hillier"/>
+          <Body>{bio.professional}</Body>
+          <Body>{bio.personal}</Body>
         </Sidebar>
         <Content>
-          <Subtitle>Content Here</Subtitle>
+          <Subtitle>My Projects</Subtitle>
           <ButtonLink href="http://google.ca" target="_blank">
             Test
           </ButtonLink>
